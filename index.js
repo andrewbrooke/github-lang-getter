4f69751a94639312914e3aa58a31e335c72df412
Andrew Brooke <andrewbrooke15@gmail.com>
2017-03-20 12:08:11 -0400

    Added support for pagination in Github API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const async = require('async');
 const detect = require('language-detect');
 const different = require('different');
 const inspector = require('schema-inspector');
+const parse = require('parse-link-header');
 const request = require('request-promise-native');
 
 const API_BASE_URL = 'https://api.github.com';
@@ -15,13 +16,14 @@ const baseOpts = {
         'Accept': 'application/vnd.github.v3+json',
         'User-Agent': 'Request-Promise'
     },
-    json: true
+    json: true,
+    resolveWithFullResponse: true
 };
 
 /**
  * Gets a Github user's repository programming language distribution
- * @param  {String} visibility Type of repositories to find (can be all, public, or private)
- * @param  {Object} token Github personal access token
+ * @param  {String} visibility  Type of repositories to find (can be all, public, or private)
+ * @param  {Object} token       Github personal access token
  * @return {Promise}            Resolves if API request performed successfully
  *                              Rejects if parameters are invalid, or error occurs with API request
  */
@@ -30,9 +32,10 @@ exports.getRepoLanguages = (visibility = 'public', token) => {
         getUserRepos(visibility, token).then((repos) => {
             var urls = _.map(repos, 'languages_url');
             // Push a function for each URL to get the languages byte count, and process them asynchronously
-            var funcs = _.map(urls, _.curry(createAPIRequestFunc)(token));
-            async.parallel(funcs, (err, results) => {
+            var funcs = _.map(urls, _.curry(createAPIRequestFunc)(token, null));
+            async.parallel(funcs, (err, responses) => {
                 if (err) reject(err);
+                var results = _.map(responses, 'body');
                 // Count bytes per language
                 var totals = {};
                 _.each(results, (obj) => {
@@ -51,8 +54,8 @@ exports.getRepoLanguages = (visibility = 'public', token) => {
 
 /**
  * Gets a Github user's repository programming language distribution
- * @param  {String} visibility Type of repositories to find (can be all, public, or private)
- * @param  {Object} token Github personal access token
+ * @param  {String} visibility  Type of repositories to find (can be all, public, or private)
+ * @param  {Object} token       Github personal access token
  * @return {Promise}            Resolves if API request performed successfully
  *                              Rejects if parameters are invalid, or error occurs with API request
  */
@@ -64,7 +67,8 @@ exports.getCommitLanguages = (visibility = 'public', token) => {
                 uri: API_BASE_URL + '/user',
                 qs: {
                     access_token: token, // eslint-disable-line
-                }
+                },
+                resolveWithFullResponse: false
             }, baseOpts);
 
             // Perform API request and handle result appropriately
@@ -73,8 +77,8 @@ exports.getCommitLanguages = (visibility = 'public', token) => {
                 var urls = _.map(repos, (repo) => {
                     return repo.url + '/commits'
                 });
-                // TODO: account for pagination in the Github API
-                var funcs = _.map(urls, _.curry(createAPIRequestFunc)(token));
+
+                var funcs = _.map(urls, _.curry(getRepoCommits)(user.login, token));
                 async.parallelPlus(funcs, (err, results) => { // eslint-disable-line
                     // Filter out undefined results
                     var commitArrays = _.filter(results, (result) => {
@@ -84,19 +88,20 @@ exports.getCommitLanguages = (visibility = 'public', token) => {
                     // For each array of commits
                     _.each(commitArrays, (commitArray) => {
                         // Filter out commits that don't belong to the user
-                        commits = commits.concat(_.filter(commitArray, (c) => {
-                            return c && c.committer && c.committer.id === user.id;
-                        }));
+                        commits = commits.concat(commitArray);
                     });
+
                     var totals = {};
 
                     // Get individual commit data from API
-                    var urls = _.map(commits, 'url');
-                    var funcs = _.map(urls, _.curry(createAPIRequestFunc)(token));
-                    async.parallel(funcs, (err, commits) => { // eslint-disable-line
+                    var urls = _.filter(_.map(commits, 'url'), (c) => {
+                        return c !== undefined;
+                    });
+                    var funcs = _.map(urls, _.curry(createAPIRequestFunc)(token, null));
+                    async.parallel(funcs, (err, responses) => { // eslint-disable-line
+                        var commits = _.map(responses, 'body');
                         _.each(commits, (commit) => {
                             _.each(commit.files, (file) => {
-                                // TODO: make sure these language names match up with the ones returned from Github API
                                 var language = detect.filename(file.filename);
                                 if (language) {
                                     // Parse Git diff
@@ -125,13 +130,12 @@ exports.getCommitLanguages = (visibility = 'public', token) => {
 
 /**
  * Gets a list of Github user's repositories from the Github API
- * @param  {String} visibility Type of repositories to find (can be all, public, or private)
- * @param  {String} token   Github personal access token
- * @return {Promise}        Resolves if repo URLs are obtained
- *                          Rejects if an error occurs obtaining URLs
+ * @param  {String} visibility  Type of repositories to find (can be all, public, or private)
+ * @param  {String} token       Github personal access token
+ * @return {Promise}            Resolves if repo URLs are obtained
+ *                              Rejects if an error occurs obtaining URLs
  */
 function getUserRepos(visibility = 'public', token) {
-    // TODO: account for pagination in the Github API
     return new Promise((resolve, reject) => {
         // First validate the user input
         var validation = {
@@ -141,17 +145,36 @@ function getUserRepos(visibility = 'public', token) {
         if (!result.valid) throw Error(result.format());
 
         // Form options for API request
+        var url = API_BASE_URL + '/user/repos';
         var options = _.defaults({
-            uri: API_BASE_URL + '/user/repos',
+            uri: url,
             qs: {
                 access_token: token, // eslint-disable-line
+                per_page: 100, // eslint-disable-line
                 visibility: visibility
             }
         }, baseOpts);
 
         // Perform API request and handle result appropriately
-        request(options).then((repos) => {
-            resolve(repos);
+        request(options).then((response) => {
+            var repos = response.body;
+            var link = parse(response.headers.link);
+            if (link) { // Get the other pages of results if necessary
+                var funcs = []; // To store the functions that we pass in to Async parallel
+                var start = Number(link.next.page);
+                var end = Number(link.last.page);
+                for (var page = start; page <= end; page++) {
+                    funcs.push(_.curry(createAPIRequestFunc)(token, page, url))
+                }
+                async.parallelPlus(funcs, (err, results) => { // eslint-disable-line
+                    _.each(results, (result) => {
+                        repos = repos.concat(result.body);
+                    });
+                    resolve(repos);
+                });
+            } else {
+                resolve(repos);
+            }
         }).catch((err) => {
             reject(err);
         });
@@ -159,25 +182,73 @@ function getUserRepos(visibility = 'public', token) {
 }
 
 /**
- * Creates and returns a function to be used to get the language makeup for a specific Github repo
- * @param  {String} token   Github personal access token
- * @param  {String} repoUrl Github repository URL
- * @return {Function}       Function to be passed into Async call with callback
+ * Creates and returns a function to be used to get all of the commits for a Github repo
+ * @param  {String} username    Github username
+ * @param  {String} token       Github personal access token
+ * @param  {String} repoUrl     Github repository URL
+ * @return {Function}           Function to be passed into Async call with callback
  */
-function createAPIRequestFunc(token, repoUrl) {
+function getRepoCommits(username, token, repoUrl) {
     return function(callback) {
         // Form options for API request
         var options = _.defaults({
             uri: repoUrl,
             qs: {
                 access_token: token, // eslint-disable-line
-                per_page: 100 // eslint-disable-line
+                per_page: 100, // eslint-disable-line
             }
         }, baseOpts);
+        if (username) options.qs.author = username;
 
         // Perform API request and handle result appropriately
-        request(options).then((result) => {
-            callback(null, result);
+        request(options).then((response) => { // eslint-disable-line
+            var repos = response.body;
+            var link = parse(response.headers.link);
+            if (link) { // Get the other pages of results if necessary
+                var funcs = []; // To store the functions that we pass in to Async parallel
+                var start = Number(link.next.page);
+                var end = Number(link.last.page);
+                for (var page = start; page <= end; page++) {
+                    funcs.push(_.curry(createAPIRequestFunc)(token, page, repoUrl))
+                }
+                async.parallelPlus(funcs, (err, results) => { // eslint-disable-line
+                    _.each(results, (result) => {
+                        repos = repos.concat(result.body);
+                    });
+
+                    return callback(null, repos);
+                });
+            } else {
+                return callback(null, repos);
+            }
+        }).catch((err) => {
+            return callback(err);
+        });
+    }
+}
+
+/**
+ * Creates and returns a function to be used to get the language makeup for a specific Github repo
+ * @param  {String} token   Github personal access token
+ * @param  {Integer} page   Used for pagination, can be undefined
+ * @param  {String} url     Github API URL
+ * @return {Function}       Function to be passed into Async call with callback
+ */
+function createAPIRequestFunc(token, page, url) {
+    return function(callback) {
+        // Form options for API request
+        var options = _.defaults({
+            uri: url,
+            qs: {
+                access_token: token, // eslint-disable-line
+                per_page: 100, // eslint-disable-line
+            }
+        }, baseOpts);
+        if (page) options.qs.page = page;
+
+        // Perform API request and handle result appropriately
+        request(options).then((response) => {
+            callback(null, response);
         }).catch((err) => {
             callback(err);
         });
@@ -186,7 +257,7 @@ function createAPIRequestFunc(token, repoUrl) {
 
 /**
  * Addition to Async parallel method to prevent function chain from breaking on failure
- * @param  {[type]}   functions List of functions to execute
+ * @param  {Array}   functions  List of functions to execute
  * @param  {Function} callback  Callback function
  * @return {Function}           Async parallel method to execute
  */

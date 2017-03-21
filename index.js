1062020ab399ce21438957c009c0ff5f6513f3ec
Andrew Brooke <andrewbrooke15@gmail.com>
2017-03-21 14:46:08 -0400

    Native Promises (#2)
    Removed async dependency
    
    * ESLint cleanup
    
    * Migrated Promises to Bluebird for getRepoLanguages
    
    * Migrated Promises to Bluebird for getCommitLanguages
    
    * Remove BB dep
    
    * ESLint cleanup

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const _ = require('lodash');
-const async = require('async');
 const detect = require('language-detect');
 const different = require('different');
 const inspector = require('schema-inspector');
@@ -16,6 +15,9 @@ const baseOpts = {
         'Accept': 'application/vnd.github.v3+json',
         'User-Agent': 'Request-Promise'
     },
+    qs: {
+        per_page: 100, // eslint-disable-line
+    },
     json: true,
     resolveWithFullResponse: true
 };
@@ -28,13 +30,22 @@ const baseOpts = {
  *                              Rejects if parameters are invalid, or error occurs with API request
  */
 exports.getRepoLanguages = (visibility, token) => {
-    return getUserRepos(visibility, token).then((repos) => {
+    // First get the User's repositories
+    return getUserRepos(visibility, token).then((responses) => {
+        // Parse the repos json from the response bodies
+        var repos = [];
+        _.each(responses, (response) => {
+            repos = repos.concat(response.body);
+        });
+
+        // Map Promises for each URL to resolve to the total language byte count
         var urls = _.map(repos, 'languages_url');
-        // Push a function for each URL to get the languages byte count, and process them asynchronously
         var promises = _.map(urls, _.curry(createAPIRequestPromise)(token, null));
+
         return Promise.all(promises);
-    }).then(responses => {
+    }).then((responses) => {
         var results = _.map(responses, 'body');
+
         // Count bytes per language
         var totals = {};
         _.each(results, (obj) => {
@@ -43,21 +54,28 @@ exports.getRepoLanguages = (visibility, token) => {
                 totals[key] += obj[key];
             });
         });
+
         return totals;
     });
 };
 
 /**
- * Gets a Github user's repository programming language distribution
+ * Gets a Github user's commit programming language distribution
  * @param  {String} visibility  Type of repositories to find (can be all, public, or private)
  * @param  {Object} token       Github personal access token
  * @return {Promise}            Resolves if API request performed successfully
  *                              Rejects if parameters are invalid, or error occurs with API request
  */
 exports.getCommitLanguages = (visibility, token) => {
-    return getUserRepos(visibility, token).then((repos) => {
-        // Get Github username from access token
-        var options = _.defaults({
+    // First get the user's repositories
+    return getUserRepos(visibility, token).then((responses) => {
+        // Parse the repos json from the response bodies
+        var repos = [];
+        _.each(responses, (response) => {
+            repos = repos.concat(response.body);
+        });
+
+        var options = _.defaultsDeep({
             uri: API_BASE_URL + '/user',
             qs: {
                 access_token: token, // eslint-disable-line
@@ -65,53 +83,62 @@ exports.getCommitLanguages = (visibility, token) => {
             resolveWithFullResponse: false
         }, baseOpts);
 
-        // Perform API request and handle result appropriately
+        // Get Github username from access token
         return request(options).then((user) => {
             // Get Repo commit URLs
             var urls = _.map(repos, (repo) => {
                 return repo.url + '/commits'
             });
 
-            var funcs = _.map(urls, _.curry(getRepoCommits)(user.login, token));
-            return new Promise((resolve, reject) => { // Promise wrapper for callback-driven routine
-                async.parallelPlus(funcs, (err, results) => { // eslint-disable-line
-                    // Filter out undefined results
-                    var commitArrays = _.filter(results, (result) => {
-                        return result !== undefined;
-                    });
-                    var commits = [];
-                    // For each array of commits
-                    _.each(commitArrays, (commitArray) => {
-                        // Filter out commits that don't belong to the user
-                        commits = commits.concat(commitArray);
-                    });
+            // Map a Promise for each repo commit URL
+            var promises = _.map(urls, _.curry(getRepoCommits)(user.login, token));
+            promises = promises.map((p) => p.then((v) => v, (e) => ({ error: e })));
 
-                    // Get individual commit data from API
-                    var urls = _.filter(_.map(commits, 'url'), (c) => {
-                        return c !== undefined;
-                    });
-                    var promises = _.map(urls, _.curry(createAPIRequestPromise)(token, null));
-                    resolve(Promise.all(promises));
-                });
-            }).then(responses => {
-                var totals = {};
-                var commits = _.map(responses, 'body');
-                    _.each(commits, (commit) => {
-                        _.each(commit.files, (file) => {
-                            var language = detect.filename(file.filename);
-                            if (language) {
-                                // Parse Git diff
-                                different.parseDiffFromString('diff\n' + file.patch, (diff) => {
-                                    // Sum number of bytes from additions and add to results
-                                    var byteCount = _.reduce(diff[0].additions, (sum, line) => {
-                                        return line.length;
-                                    }, 0);
-                                    if (!totals[language]) totals[language] = 0;
-                                    totals[language] += byteCount;
-                                });
-                            }
+            return Promise.all(promises).then((results) => {
+                var commits = [];
+
+                // Get commits from Promise reponses
+                results.forEach((result) => {
+                    if (result.error) {
+                        // TODO: Promise may be rejected in certain cases should we do anything here?
+                    } else {
+                        _.each(result, (value) => {
+                            commits = commits.concat(value.body);
                         });
+                    }
+                });
+
+                // Map a promise for each individual commit URL
+                var urls = _.chain(commits).filter((c) => {
+                                return c && c.author && c.author.login === user.login;
+                            }).map('url').value();
+
+                var promises = _.map(urls, _.curry(createAPIRequestPromise)(token, null));
+
+                return Promise.all(promises);
+            }).then((responses) => {
+                var commits = _.map(responses, 'body');
+                var totals = {};
+
+                // For each file in the commit files
+                _.each(commits, (commit) => {
+                    _.each(commit.files, (file) => {
+                        var language = detect.filename(file.filename);
+                        if (language) {
+                            // Parse Git diff
+                            different.parseDiffFromString('diff\n' + file.patch, (diff) => {
+                                // Sum number of bytes from additions and add to results
+                                var byteCount = _.reduce(diff[0].additions, (sum, line) => {
+                                    return line.length;
+                                }, 0);
+
+                                if (!totals[language]) totals[language] = 0;
+                                totals[language] += byteCount;
+                            });
+                        }
                     });
+                });
+
                 return totals;
             });
         });
@@ -126,7 +153,7 @@ exports.getCommitLanguages = (visibility, token) => {
  *                              Rejects if an error occurs obtaining URLs
  */
 function getUserRepos(visibility, token) {
-    // First validate the user input
+    // First validate the user token input
     var validation = {
         type: 'string'
     };
@@ -135,148 +162,97 @@ function getUserRepos(visibility, token) {
 
     // Form options for API request
     var url = API_BASE_URL + '/user/repos';
-    var options = _.defaults({
+    var options = _.defaultsDeep({
         uri: url,
         qs: {
             access_token: token, // eslint-disable-line
-            per_page: 100, // eslint-disable-line
             visibility: visibility
         }
     }, baseOpts);
 
-    // Perform API request and handle result appropriately
-    return request(options).then((response) => {
-        var repos = response.body;
+    return request(options).then((response) => { // eslint-disable-line
         var link = parse(response.headers.link);
+        var promises = []; // To store the promises to resolve the other pages of repos
+
         if (link) { // Get the other pages of results if necessary
-            var funcs = []; // To store the functions that we pass in to Async parallel
-            var start = Number(link.next.page);
-            var end = Number(link.last.page);
+            var start = Number(link.next.page), end = Number(link.last.page);
             for (var page = start; page <= end; page++) {
-                funcs.push(_.curry(createAPIRequestFunc)(token, page, url))
+                promises.push(_.curry(createAPIRequestPromise)(token, {
+                    page: page,
+                    visibility: visibility
+                }, url));
             }
-            async.parallelPlus(funcs, (err, results) => { // eslint-disable-line
-                _.each(results, (result) => {
-                    repos = repos.concat(result.body);
-                });
-                return repos;
-            });
         }
-        return repos;
+        promises.push(_.curry(createAPIRequestPromise)(token, {
+            page: 1,
+            visibility: visibility
+        }, url));
+
+        return Promise.all(promises);
     });
 }
 
 /**
- * Creates and returns a function to be used to get all of the commits for a Github repo
+ * Creates and returns a promise to resolve to all of the commits for a Github repo
  * @param  {String} username    Github username
  * @param  {String} token       Github personal access token
  * @param  {String} repoUrl     Github repository URL
- * @return {Function}           Function to be passed into Async call with callback
+ * @return {Promise}            Promise to resolve repo commits
  */
 function getRepoCommits(username, token, repoUrl) {
-    return function(callback) {
-        // Form options for API request
-        var options = _.defaults({
-            uri: repoUrl,
-            qs: {
-                access_token: token, // eslint-disable-line
-                per_page: 100, // eslint-disable-line
-            }
-        }, baseOpts);
-        if (username) options.qs.author = username;
+    // Form options for API request
+    var options = _.defaultsDeep({
+        uri: repoUrl,
+        qs: {
+            access_token: token, // eslint-disable-line
+        }
+    }, baseOpts);
+    if (username) options.qs.author = username;
 
-        // Perform API request and handle result appropriately
-        request(options).then((response) => { // eslint-disable-line
-            var repos = response.body;
-            var link = parse(response.headers.link);
-            if (link) { // Get the other pages of results if necessary
-                var funcs = []; // To store the functions that we pass in to Async parallel
-                var start = Number(link.next.page);
-                var end = Number(link.last.page);
-                for (var page = start; page <= end; page++) {
-                    funcs.push(_.curry(createAPIRequestFunc)(token, page, repoUrl));
-                }
-                async.parallelPlus(funcs, (err, results) => { // eslint-disable-line
-                    _.each(results, (result) => {
-                        repos = repos.concat(result.body);
-                    });
+    return request(options).then((response) => { // eslint-disable-line
+        var promises = []; // To store the promises to resolve the other pages of commits
+        var link = parse(response.headers.link);
 
-                    return callback(null, repos);
-                });
-            } else {
-                return callback(null, repos);
+        if (link) { // Get the other pages of results if necessary
+            var start = Number(link.next.page), end = Number(link.last.page);
+            for (var page = start; page <= end; page++) {
+                promises.push(_.curry(createAPIRequestPromise)(token, {
+                    page: page,
+                    author: username
+                }, repoUrl));
             }
-        }).catch((err) => {
-            return callback(err);
-        });
-    }
+        }
+        promises.push(_.curry(createAPIRequestPromise)(token, {
+            page: 1,
+            author: username
+        }, repoUrl));
+
+        return Promise.all(promises);
+    });
 }
 
 /**
- * Creates and returns a function to be used to get the language makeup for a specific Github repo
+ * Creates and returns a promise to resolve to a specific Github API request result
  * @param  {String} token   Github personal access token
- * @param  {Integer} page   Used for pagination, can be undefined
+ * @param  {Object} qs      Extra query string parameters
  * @param  {String} url     Github API URL
- * @return {Promise}        Promise to be passed into Promise.all()
+ * @return {Promise}        Promise to be resolved somewhere
  */
-function createAPIRequestPromise(token, page, url) {
+function createAPIRequestPromise(token, qs, url) {
     // Form options for API request
-    var options = _.defaults({
+    var options = _.defaultsDeep({
         uri: url,
         qs: {
             access_token: token, // eslint-disable-line
-            per_page: 100, // eslint-disable-line
         }
     }, baseOpts);
-    if (page) options.qs.page = page;
+    if (qs) {
+        for (var key in qs) {
+            if (Object.prototype.hasOwnProperty.call(qs, key))
+                options.qs[key] = qs[key];
+        }
+    }
 
     // Perform API request and handle result appropriately
     return request(options);
-}
-
-function createAPIRequestFunc(token, page, url) {
-    return function(callback) {
-        // Form options for API request
-        var options = _.defaults({
-            uri: url,
-            qs: {
-                access_token: token, // eslint-disable-line
-                per_page: 100, // eslint-disable-line
-            }
-        }, baseOpts);
-        if (page) options.qs.page = page;
-
-        // Perform API request and handle result appropriately
-        request(options).then((response) => {
-            callback(null, response);
-        }).catch((err) => {
-            callback(err);
-        });
-    }
-}
-
-/**
- * Addition to Async parallel method to prevent function chain from breaking on failure
- * @param  {Array}   functions  List of functions to execute
- * @param  {Function} callback  Callback function
- * @return {Function}           Async parallel method to execute
- */
-async.parallelPlus = function(functions, callback) {
-    function wrap(func) {
-        return function(callback) {
-            func(function(err, value) {
-                if (err) return callback(null, false);
-
-                return callback(null, value);
-            });
-        }
-    }
-    var newFunctions = {};
-    for (var func in functions) {
-        if (functions.hasOwnProperty(func)) {
-            newFunctions[func] = wrap(functions[func]);
-        }
-    }
-
-    return async.parallel(newFunctions, callback);
 }
